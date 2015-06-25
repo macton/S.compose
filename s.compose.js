232b160a2a565d55226e1ae8d2f73632d875d911
Mike Acton <macton@gmail.com>
2015-06-25 10:35:46 -0700

    add spread

--- s.compose.js
+++ s.compose.js
@@ -1,10 +1,11 @@
 var S = require('s');
 
 var compose = {
-  R     : S.curry( function( list, value ) { return list.reduceRight( function( prev, next ) { return S.cv( next, prev ); }, value ); }),
-  L     : S.curry( function( list, value ) { return list.reduce( function( prev, next ) { return S.cv( next, prev ); }, value ); }),
-  tap   : function( fn ) { return function( a ) { fn(a); return a; }; },
-  flip  : S.curry( function( fn, a, b ) { return fn(b,a); }),
+  R      : S.curry( function( list, value ) { return list.reduceRight( function( prev, next ) { return S.cv( next, prev ); }, value ); }),
+  L      : S.curry( function( list, value ) { return list.reduce( function( prev, next ) { return S.cv( next, prev ); }, value ); }),
+  tap    : function( fn ) { return function( a ) { fn(a); return a; }; },
+  flip   : S.curry( function( fn, a, b ) { return fn(b,a); }),
+  spread : S.curry( function( fn, arr ) { return fn.apply( null, arr ); }),
 };
 
 exports = module.exports = compose;
