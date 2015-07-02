08f9bf845fc9979ffaf86381047551d9546a296b
Mike Acton <macton@gmail.com>
2015-07-01 18:45:06 -0700

    add rest

--- s.compose.js
+++ s.compose.js
@@ -6,6 +6,7 @@ var compose = {
   tap    : function( fn ) { return function( a ) { fn(a); return a; }; },
   flip   : S.curry( function( fn, a, b ) { return fn(b,a); }),
   spread : S.curry( function( fn, arr ) { return fn.apply( null, arr ); }),
+  rest   : S.curry( function( ndx ) { return function() { return Array.prototype.slice.call(arguments, ndx); }; }), 
 };
 
 exports = module.exports = compose;
