316e6672787ac1f9392c7e1dabc96b246e2dd2c1
Mike Acton <macton@gmail.com>
2015-07-13 17:16:24 -0700

    add zip

--- s.compose.js
+++ s.compose.js
@@ -7,6 +7,13 @@ var compose = {
   flip   : S.curry( function( fn, a, b ) { return fn(b,a); }),
   spread : S.curry( function( fn, arr ) { return fn.apply( null, arr ); }),
   rest   : S.curry( function( ndx ) { return function() { return Array.prototype.slice.call(arguments, ndx); }; }), 
+  zip    : S.curry( function( a, b ) {
+    var result = [];
+    for (var i=0;i<a.length;i++) {
+      result[i] = S.cv( a[i], b[i] );
+    }
+    return result;
+  }),
 };
 
 exports = module.exports = compose;
